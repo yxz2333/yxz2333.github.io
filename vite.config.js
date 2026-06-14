604d2526d6556dd15e5061477450e6deb11b0fdc
yxz2333 <1467233422@qq.com>
2026-06-15 04:16:09 +0800

    change base to /

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Lynia-s-Blog/' : '/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
